690c57031fcd46b4b24444d8e86450b7801b49d0
JaQwae <105896967+JaQwae@users.noreply.github.com>
2024-03-01 16:08:23 -0800

    constructed the backbone of the nav menu

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,40 +1,29 @@
-import React from 'react'
+import React from 'react';
+import "./Navbar.css";
 
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">Navbar</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">About</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Services</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Gallery</a>
-            </li>
-            <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Resources
-              </a>
-              <ul className="dropdown-menu">
-                <li><a className="dropdown-item" href="#">Blog</a></li>
-                <hr className="dropdown-divider"/>
-                <li><a className="dropdown-item" href="#">News</a></li>
-                <hr className="dropdown-divider"/>
-                <li><a className="dropdown-item" href="#">Promotions</a></li>
-              </ul>
-            </li>
-          </ul>
-        </div>
-      </div>
+    <nav className="navbar">
+      <ul className='nav'>
+          <img 
+            src="https://www.accentonprint.com/news/wp-content/uploads/2017/01/nike-logo-copy.jpg" 
+            alt="Company-Logo" 
+            id="company-logo"/>
+          <div id='navbar-content'>
+            <div id="nav-links-section" className='navbar-content-section'>
+              <li className="nav-links nav-elements">About</li>
+              <li className="nav-links nav-elements">Services</li>
+              <li className="nav-links nav-elements">Gallery</li>
+              <li className="nav-links nav-elements">Resources</li>
+            </div>
+            <div id="nav-contact-section" className='navbar-content-section'>
+              <li className='nav-elements'>(555)-555-5555</li>
+              <button id="nav-request-service-btn" className='nav-elements'>Request Service</button>
+            </div>
+          </div>
+          
+      </ul>
     </nav>
   )
 }
