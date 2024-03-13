4b533ad9e49eb8a34b8b14b2bf2de08344522aa8
JaQwae <105896967+JaQwae@users.noreply.github.com>
2024-03-13 14:58:14 -0700

    skeleton created for blog page

--- client/src/pages/Blog/Blog.jsx
+++ client/src/pages/Blog/Blog.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import BlogContainer from '../../components/blog-container/BlogContainer'
+import "./Blog.css"
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div id="blog-page">
+      <div className="page-header">
+        <h1 id="blog-header">Blog</h1>
+        <hr className='header-line' />
+      </div>
+      <div id="blog-container">
+        <BlogContainer/>
+      </div>
+    </div>
   )
+
 }
 
 export default Blog
