b12ee36a1e29a0a35fdd4721fe9c19cc51979bbd
JaQwae <105896967+JaQwae@users.noreply.github.com>
2024-03-19 14:26:29 -0700

    general tablet and  mobile styling for home page completed

--- client/src/components/review/Review.jsx
+++ client/src/components/review/Review.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { PropTypes } from "prop-types";
 import googleReviewLogo from "../../assets/images/home-images/googleReviewLogo.png"
 import googleStars from "../../assets/images/home-images/googleStars.png"
+import "./Review.css"
 
 const Review = (props) => {
 
@@ -25,10 +26,10 @@ const Review = (props) => {
                 <p
                     className='customer-review'
                 >
-                    {reviewData.review}
+                    {`"${reviewData.review}"`}
                 </p>
                 <p className='customer-name'>
-                    {reviewData.name}
+                    {`- ${reviewData.name}`}
                 </p>
             </div>
         </>
