d185c21ff91145c3d1933a98b2af2d46826e9a79
JaQwae <105896967+JaQwae@users.noreply.github.com>
2024-03-22 15:36:27 -0700

    skelton styling for gallery created

--- client/src/pages/Gallery/Gallery.jsx
+++ client/src/pages/Gallery/Gallery.jsx
@@ -6,7 +6,7 @@ const Gallery = () => {
   return (
     <div id="gallery-page">
       <div className="page-header">
-        <h1 id="gallery-header">Gallery</h1>
+        <h1 id="gallery-header" className='page-title'>Gallery</h1>
         <hr className='header-line'/>
       </div>
       <div id="photo-container">
