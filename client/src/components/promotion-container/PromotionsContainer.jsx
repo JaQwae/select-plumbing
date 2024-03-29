a3159b9bbbd1d191ca1398c31b37ae5e6d1a9229
JaQwae <105896967+JaQwae@users.noreply.github.com>
2024-03-29 14:31:41 -0700

    responsive styling added for promotions page

--- client/src/components/promotion-container/PromotionsContainer.jsx
+++ client/src/components/promotion-container/PromotionsContainer.jsx
@@ -11,7 +11,14 @@ const PromotionsContainer = () => {
                     <h3 className="promotion-title">{item.title}</h3>
                     <p className="promotion-discount">{item.discount}</p>
                     <p className="promotion-summary">{item.summary}</p>
+                    <a
+                      href="https://form.jotform.com/240635777814162"
+                      target= '_blank'
+                      rel= 'noopener noreferrer'
+                      className='btn-link'
+                    >
                     <button className="secondary-btn btn">Request Service</button>
+                  </a>
                 </div>
             </section>
         ))}
--- client/src/pages/Promotions/Promotions.jsx
+++ client/src/pages/Promotions/Promotions.jsx
@@ -6,8 +6,8 @@ const Promotions = () => {
   return (
     <div className='promotions-page'>
       <div className="page-header">
-        <h1 id="promotions-header">Promotions</h1>
-        <hr className='header-line' />
+        <h1 id="promotions-header" className="page-title">Promotions</h1>
+        {/* <hr className='header-line' /> */}
       </div>
       <div id="promotions-container">
         <PromotionsContainer/>
