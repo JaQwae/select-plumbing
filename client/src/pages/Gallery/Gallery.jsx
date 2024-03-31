4414906ae2d12308c03e0de38380f26b5a1b8ee2
JaQwae <105896967+JaQwae@users.noreply.github.com>
2024-03-31 12:06:53 -0700

    updated fonts on website

--- client/src/pages/Gallery/Gallery.jsx
+++ client/src/pages/Gallery/Gallery.jsx
@@ -8,7 +8,7 @@ const Gallery = () => {
     <div id="gallery-page">
       <div className="page-header">
         <h1 id="gallery-header" className='page-title'>Gallery</h1>
-        <hr className='header-line'/>
+        {/* <hr className='header-line'/> */}
       </div>
       <div id="photo-container">
         <GalleryPhoto/>
